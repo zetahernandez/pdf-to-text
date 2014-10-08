7629909de35c091a6495896bda3021fd551b8c7c
Fernando Hernandez <zetahernandez@gmail.com>
2014-10-08 04:36:20 -0300

    fix scope

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ var info = require('./lib/info.js'),
   extract = require('./lib/extract-text.js'),
   fs = require('fs');
 
+function PDF() {
+  if (false === (this instanceof PDF)) {
+    return new PDF();
+  }
+}
 
-module.exports.info = function(pdf_path, cb) {
+PDF.prototype.info = function(pdf_path, cb) {
 
   fs.exists(pdf_path, function(exist) {
     if (!exist) return cb('no file exists at the path you specified');
     info.process(pdf_path, cb);
   });
 }
-module.exports.pdfToText = function(pdf_path, options, cb) {
+PDF.prototype.pdfToText = function(pdf_path, options, cb) {
 
   fs.exists(pdf_path, function(exist) {
     if (!exist) return cb('no file exists at the path you specified');
     extract.process(pdf_path, options, cb);
   });
 }
+
+module.exports = new PDF();
